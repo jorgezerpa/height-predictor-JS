7147b576dc0b1c2d574f0b85782964e90c19ce4b
jorgezerpa <jorgezerpacoder@gmail.com>
2022-02-02 22:38:59 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+function main(){
+	window.addEventListener("deviceorientation", onOrientationChange);
+
+	navigator.mediaDevices.getDisplayMedia({video:true})
+		.then((signal)=>{
+			const video=document.getElementById('my_video');
+			video.srcObject=signal;
+			video.play();
+		})
+		.catch(err=>alert(err));
+}
+
+
+
+function onOrientationChange(event){
+	let angle = event.beta-90;
+	if(angle<0){
+		angle=0;
+	}
+
+	const distanceToObject= document.getElementById('my_distance').value;
+	console.log(distanceToObject);
+	document.getElementById('show_my_distance').innerHTML= 
+				"distance to object:"+distanceToObject+"meters";
+
+
+	const height= Math.tan(angle*Math.PI/180)*distanceToObject;
+
+	document.getElementById("heightInfo").innerHTML=
+		height.toFixed(1)+"m ("+ angle.toFixed(1)+"&deg)";
+
+}
